6873a21e0fbeafab7255b68b4c048d4835f7b0f3
adit1 <adit10022004@gmail.com>
2026-02-05 20:58:39 +0530

    viemodel layer

---
+++ app/src/main/java/viewmodel/UserViewModel.kt
@@ -0,0 +1,29 @@
+package viewmodel
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import datalayer.RetrofitInstance
+import datalayer.User
+import kotlinx.coroutines.launch
+
+class UserViewModel : ViewModel() {
+    var users = mutableStateOf<List<User>>(emptyList())
+    var isLoading = mutableStateOf(false)
+    var errorMessage = mutableStateOf<String?>(null)
+
+    init { fetchUsers() }
+
+    fun fetchUsers() {
+        viewModelScope.launch {
+            isLoading.value = true
+            errorMessage.value = null
+            try {
+                users.value = RetrofitInstance.api.getUsers()
+            } catch (e: Exception) {
+                errorMessage.value = "Failed to fetch users. Check internet."
+            } finally {
+                isLoading.value = false
+        }
+    }
+}}
