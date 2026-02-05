326a88b175b93bc737e4c1691c01c6d6c2f20396
adit1 <adit10022004@gmail.com>
2026-02-05 21:33:06 +0530

    imports

--- app/src/main/java/com/example/userpulse/MainActivity.kt
+++ app/src/main/java/com/example/userpulse/MainActivity.kt
@@ -10,8 +10,13 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.userpulse.ui.theme.UserpulseTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import datalayer.User
+import viewmodel.UserViewModel
+import kotlin.getValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
